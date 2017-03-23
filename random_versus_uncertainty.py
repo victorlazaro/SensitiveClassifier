bec36048b7f54a473b7a39624f43d4afd34234a7
Connor Cook <cojoco@users.noreply.github.com>
2017-03-23 15:31:44 -0600

    Added some print statements to let the user know what's going on

--- random_versus_uncertainty.py
+++ random_versus_uncertainty.py
@@ -132,18 +132,41 @@ def main():
                                                  random_strat,
                                                  quota)
 
-    query_num = np.arange(1, quota+1)
+    # query_num = np.arange(1, quota+1)
     # plt.plot(query_num, train_error_uncertainty, 'b', label='Uncertainty Train')
-    plt.plot(query_num, test_error_uncertainty, 'r', label='Uncertainty Test')
+    # plt.plot(query_num, test_error_uncertainty, 'r', label='Uncertainty Test')
     # plt.plot(query_num, train_error_random, 'g', label='Random Train')
-    plt.plot(query_num, test_error_random, 'k', label='Random Test')
+    # plt.plot(query_num, test_error_random, 'k', label='Random Test')
+    # plt.xlabel('Number of Queries')
+    # plt.ylabel('Error')
+    # plt.title('Uncertainty Sampling vs Random Sampling')
+    # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+    #            fancybox=True, shadow=True, ncol=5)
+    # plt.show()
+    return test_error_uncertainty, test_error_random, quota
+
+
+if __name__ == '__main__':
+    num_experiments = 100
+    print('Running the experiment 100 times and averaging the results...')
+    print('Please wait...')
+    uncertain_test_errors, random_test_errors, quota = main()
+    for i in range(num_experiments-1):
+        if (i+1) % 10 == 0:
+            print('finished', str(i+1), 'experiments...')
+        test_error_uncertainty, test_error_random, _ = main()
+        # This should add the elements of the arrays together element-wise
+        uncertain_test_errors += test_error_uncertainty
+        random_test_errors += test_error_random
+    # This should divide the elements of the array element-wise
+    uncertain_test_errors /= 100
+    random_test_errors /= 100
+    query_num = np.arange(1, quota+1)
+    plt.plot(query_num, uncertain_test_errors, 'r', label='Uncertainty Test')
+    plt.plot(query_num, random_test_errors, 'k', label='Random Test')
     plt.xlabel('Number of Queries')
     plt.ylabel('Error')
     plt.title('Uncertainty Sampling vs Random Sampling')
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
                fancybox=True, shadow=True, ncol=5)
     plt.show()
-
-
-if __name__ == '__main__':
-    main()
